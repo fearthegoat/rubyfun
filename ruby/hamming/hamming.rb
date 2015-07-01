336deeaf1c0c166dd47fc58db2d6615b52451756
fearthegoat <kmanuel09@gmail.com>
2015-07-01 08:47:35 -0400

    passes 12 tests

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -3,11 +3,20 @@ class Hamming
 	a = []
 	b = []
 	c = []
+	sum = 0
 	a = x.split("") 
-	a.map! {|string| string.ord}
+	a.map! {|string| string.ord.to_i}
 	b = y.split("")
-	b.map! {|string| string.ord}
-	c = a - b
-	c.size
+	b.map! {|string| string.ord.to_i}
+	# puts "a #{a}"
+	# puts ""
+	# puts "b #{b}"
+	c = a.zip(b).map {|e, d| d - e}
+	# puts "c #{c}"
+	c.map! {|number| number == 0 ? 0 : 1} 
+	sum = c.inject(0){|t,result| t + result}
+	sum
+	# puts "sum #{sum}"
 	end
 end
+# Hamming.compute('AATG', 'AAA')
