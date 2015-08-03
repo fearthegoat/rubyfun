9841261510ab8978507d94ec18e60838211a3cd8
fearthegoat <kmanuel09@gmail.com>
2015-08-03 00:17:48 -0400

    refactored out the inject

--- prediction.rb
+++ prediction.rb
@@ -18,12 +18,11 @@ weights = [0.5,0.35,0.15]
 
 current_month = Date::MONTHNAMES[Date.today.month].downcase
 i = 3
-weighted_averages = []
+weighted_averages = 0
 while i >0
   month = Date::MONTHNAMES[Date.today.month-i].downcase
-  weighted_averages << ( THIS_YEAR[month].to_f / LAST_YEAR[month] ) * weights[i-1]
+  weighted_averages += ( THIS_YEAR[month].to_f / LAST_YEAR[month] ) * weights[i-1]
 i -= 1
 end
-weighted_average = weighted_averages.inject(0.0){ |result, averages| result + averages}
-THIS_YEAR[current_month] = (LAST_YEAR[current_month] * weighted_average).round(0)
+THIS_YEAR[current_month] = (LAST_YEAR[current_month] * weighted_averages).round(0)
 puts "#{current_month}: #{THIS_YEAR[current_month]}"
